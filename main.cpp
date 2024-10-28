d306f0f8f9ca494644c3935e26673eaf1d1ce108
mike <72279453+belikemikeunderscore@users.noreply.github.com>
2024-10-28 16:50:05 -0100

    Create main.cpp
    initial commit - base coda

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+// 4 ".h"s, 4 ".cpp"s
+
+using namespace std;
+
+struct player{
+    string id;
+    string character;
+    string status;
+};
+
+enum status {
+    alive = 1,
+    dead = 2,
+    sleeping =3
+};
+
+class common{
+    private:
+        setstatus
+        getstatus
+
+        getid
+        setid
+
+        vote()
+
+        sleep()
+        wake()
+        die()
+}
+
+class wolf{
+    bool status;
+};
+
+class seer{
+    bool status;
+};
+
+class villager{
+    bool status;
+
+};
